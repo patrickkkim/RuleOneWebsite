07dee586f1df29ff8df6f9ae02589c456a2c02aa
patrickkkim <patkim97@gmail.com>
2023-06-13 22:26:41 +0900

    Added Subscription Repository with unit tests

---
+++ src/main/java/com/valueinvesting/ruleone/repositories/SubscriptionRepository.java
@@ -0,0 +1,35 @@
+package com.valueinvesting.ruleone.repositories;
+
+import com.valueinvesting.ruleone.entities.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.Instant;
+import java.util.Optional;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
+
+    Optional<Subscription> findByAppUserId(int app_user_id);
+
+    @Modifying
+    @Query("UPDATE Subscription s SET s.subscriptionType = :type WHERE s.id = :id")
+    void updateSubscriptionTypeById(@Param("id") int id,
+                                  @Param("type")String type);
+
+    @Modifying
+    @Query("UPDATE Subscription s SET s.subscribedDate = :subscribedDate WHERE s.id = :id")
+    void updateSubscribedDateById(@Param("id") int id,
+                                  @Param("subscribedDate")Instant subscribedDate);
+
+    @Modifying
+    @Query("UPDATE Subscription s SET s.endDate = :endDate WHERE s.id = :id")
+    void updateEndDateById(@Param("id") int id,
+                                  @Param("endDate")Instant endDate);
+
+    @Modifying
+    @Query("UPDATE Subscription s SET s.purchaseType = :purchaseType WHERE s.id = :id")
+    void updatePurchaseTypeById(@Param("id") int id,
+                           @Param("purchaseType")String purchaseType);
+}
---
+++ src/test/java/com/valueinvesting/ruleone/repositories/SubscriptionRepositoryTest.java
@@ -0,0 +1,130 @@
+package com.valueinvesting.ruleone.repositories;
+
+import com.valueinvesting.ruleone.entities.AppUser;
+import com.valueinvesting.ruleone.entities.Subscription;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class SubscriptionRepositoryTest {
+
+    @Autowired private TestEntityManager testEntityManager;
+    @Autowired private SubscriptionRepository underTest;
+
+    @Test
+    void checkIfFoundByAppUserId() {
+        AppUser appUser = new AppUser();
+        appUser.setUsername("honggilddong");
+        appUser.setEmail("a@a.com");
+        appUser.setEncryptedPassword("asdfasdfasdfasdf");
+        Subscription subscription = new Subscription();
+        subscription.setSubscriptionType("trial");
+        subscription.setAppUser(appUser);
+
+        underTest.save(subscription);
+        Optional<Subscription> optionalSubscription = underTest.findByAppUserId(appUser.getId());
+        assertThat(optionalSubscription.isPresent()).isTrue();
+        Subscription found = optionalSubscription.get();
+        assertThat(found).isEqualTo(subscription);
+    }
+
+    @Test
+    void checkIfNotFoundByAppUserId() {
+        AppUser appUser = new AppUser();
+        appUser.setUsername("honggilddong");
+        appUser.setEmail("a@a.com");
+        appUser.setEncryptedPassword("asdfasdfasdfasdf");
+        Subscription subscription = new Subscription();
+        subscription.setSubscriptionType("trial");
+        subscription.setAppUser(appUser);
+
+        underTest.save(subscription);
+        Optional<Subscription> optionalSubscription = underTest.findByAppUserId(appUser.getId() + 1);
+        assertThat(optionalSubscription.isPresent()).isFalse();
+    }
+
+    @Test
+    void updateSubscriptionTypeById() {
+        AppUser appUser = new AppUser();
+        appUser.setUsername("honggilddong");
+        appUser.setEmail("a@a.com");
+        appUser.setEncryptedPassword("asdfasdfasdfasdf");
+        Subscription subscription = new Subscription();
+        subscription.setSubscriptionType("trial");
+        subscription.setAppUser(appUser);
+        String newSubscriptionType = "essential";
+
+        int id = underTest.save(subscription).getId();
+        underTest.updateSubscriptionTypeById(id, newSubscriptionType);
+        testEntityManager.refresh(subscription);
+
+        Subscription found = underTest.findById(id).get();
+        assertThat(found.getSubscriptionType()).isEqualTo(newSubscriptionType);
+    }
+
+    @Test
+    void updateSubscribedDateById() {
+        AppUser appUser = new AppUser();
+        appUser.setUsername("honggilddong");
+        appUser.setEmail("a@a.com");
+        appUser.setEncryptedPassword("asdfasdfasdfasdf");
+        Subscription subscription = new Subscription();
+        subscription.setSubscriptionType("trial");
+        subscription.setAppUser(appUser);
+        subscription.setSubscribedDate(Instant.now());
+        Instant newSubscribedDate = Instant.ofEpochSecond(Instant.now().getEpochSecond() + 100000);
+
+        int id = underTest.save(subscription).getId();
+        underTest.updateSubscribedDateById(id, newSubscribedDate);
+        testEntityManager.refresh(subscription);
+
+        Subscription found = underTest.findById(id).get();
+        assertThat(found.getSubscribedDate()).isEqualTo(newSubscribedDate);
+    }
+
+    @Test
+    void updateEndDateById() {
+        AppUser appUser = new AppUser();
+        appUser.setUsername("honggilddong");
+        appUser.setEmail("a@a.com");
+        appUser.setEncryptedPassword("asdfasdfasdfasdf");
+        Subscription subscription = new Subscription();
+        subscription.setSubscriptionType("trial");
+        subscription.setAppUser(appUser);
+        subscription.setEndDate(Instant.now());
+        Instant newEndDate = Instant.ofEpochSecond(Instant.now().getEpochSecond() + 100000);
+
+        int id = underTest.save(subscription).getId();
+        underTest.updateEndDateById(id, newEndDate);
+        testEntityManager.refresh(subscription);
+
+        Subscription found = underTest.findById(id).get();
+        assertThat(found.getEndDate()).isEqualTo(newEndDate);
+    }
+
+    @Test
+    void updatePurchaseTypeById() {
+        AppUser appUser = new AppUser();
+        appUser.setUsername("honggilddong");
+        appUser.setEmail("a@a.com");
+        appUser.setEncryptedPassword("asdfasdfasdfasdf");
+        Subscription subscription = new Subscription();
+        subscription.setSubscriptionType("trial");
+        subscription.setAppUser(appUser);
+        String newPurchaseType = "paypal";
+
+        int id = underTest.save(subscription).getId();
+        underTest.updatePurchaseTypeById(id, newPurchaseType);
+        testEntityManager.refresh(subscription);
+
+        Subscription found = underTest.findById(id).get();
+        assertThat(found.getPurchaseType()).isEqualTo(newPurchaseType);
+    }
+}
