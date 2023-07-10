bc0ef3214f8abb57f4c32a92f1f48f053b79d6e5
patrickkkim <patkim97@gmail.com>
2023-07-10 21:53:14 +0900

    temporary save for JournalService

--- src/main/java/com/valueinvesting/ruleone/services/JournalService.java
+++ src/main/java/com/valueinvesting/ruleone/services/JournalService.java
@@ -2,14 +2,28 @@ package com.valueinvesting.ruleone.services;
 
 import com.valueinvesting.ruleone.entities.AppUser;
 import com.valueinvesting.ruleone.entities.Journal;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 import java.util.Map;
 
 public interface JournalService {
-    Journal createJournal(Journal journal);
+    Journal createJournal(@NotNull Journal journal);
 
-    List<Journal> getJournalByAppUser(AppUser appUser);
+    float computeROICGrowthRate(List<Float> roicList, int years);
+
+    float computeGrowthRate(float previousValue, float currentValue, int years);
+
+    Map<String, Object> getBigFiveGrowthNumbers(@NotNull Map<String, Object> bigFiveNumbers);
+
+    float getStickerPrice(Map<String, Object> bigFiveGrowthNumbers);
+
+    Page<Journal> getPaginatedJournals(@NotNull AppUser appUser, int page, int size);
+
+    Map<String, Object> getTotalStockPercentage(@NotNull AppUser appUser);
+
+    int getTotalGain(AppUser appUser);
 
     void updateJsonBigFiveNumberByJournalId(int journalId, Map<String, Object> jsonBigFiveNumber);
 
--- src/main/java/com/valueinvesting/ruleone/services/JournalServiceImpl.java
+++ src/main/java/com/valueinvesting/ruleone/services/JournalServiceImpl.java
@@ -3,12 +3,18 @@ package com.valueinvesting.ruleone.services;
 import com.valueinvesting.ruleone.entities.AppUser;
 import com.valueinvesting.ruleone.entities.Journal;
 import com.valueinvesting.ruleone.exceptions.JournalAlreadyExistException;
+import com.valueinvesting.ruleone.exceptions.JournalInvalidException;
 import com.valueinvesting.ruleone.exceptions.JournalNotFoundException;
 import com.valueinvesting.ruleone.repositories.JournalRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -25,17 +31,55 @@ public class JournalServiceImpl implements JournalService {
 
     @Transactional
     @Override
-    public Journal createJournal(Journal journal) {
+    public Journal createJournal(@NotNull Journal journal) {
         Optional<Journal> optional = journalRepository.findById(journal.getId());
         if (optional.isPresent()) {
             throw new JournalAlreadyExistException("Journal already exists with ID: " + optional.get().getId());
+        }
+        if (journal.getAppUser() == null) throw new JournalInvalidException("AppUser cannot be null");
+        if (journal.getJsonBigFiveNumber() == null) throw new JournalInvalidException("JSON cannot be null");
+        if (journal.getTickerSymbol() == null) throw new JournalInvalidException("Stock symbol should not be null");
+        if (journal.getStockAmount() <= 0) {
+            throw new JournalInvalidException("Stock amount should be bigger than 0");
         }
         return journalRepository.save(journal);
     }
 
     @Override
-    public List<Journal> getJournalByAppUser(AppUser appUser) {
-        return journalRepository.findJournalByAppUserId(appUser.getId());
+    public float computeROICGrowthRate(List<Float> roicList, int years) {
+        return 0;
+    }
+
+    @Override
+    public float computeGrowthRate(float previousValue, float currentValue, int years) {
+        return 0;
+    }
+
+    @Override
+    public Map<String, Object> getBigFiveGrowthNumbers(@NotNull Map<String, Object> bigFiveNumbers) {
+        return null;
+    }
+
+    @Override
+    public float getStickerPrice(Map<String, Object> bigFiveGrowthNumbers) {
+        return 0;
+    }
+
+    @Override
+    public Page<Journal> getPaginatedJournals(@NotNull AppUser appUser, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return journalRepository.findJournalByAppUserId(appUser.getId(), pageable);
+    }
+
+    @Override
+    public Map<String, Object> getTotalStockPercentage(@NotNull AppUser appUser) {
+        journalRepository.findAll();
+        return null;
+    }
+
+    @Override
+    public int getTotalGain(AppUser appUser) {
+        return 0;
     }
 
     @Transactional
