d644f1a97f0b4effd812be78007f62041551286a
patrickkkim <patkim97@gmail.com>
2023-08-21 13:13:36 +0900

    Set @NotNull for setter methods

--- src/main/java/com/valueinvesting/ruleone/entities/Journal.java
+++ src/main/java/com/valueinvesting/ruleone/entities/Journal.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -28,12 +29,17 @@ public class Journal {
     private Instant lastEditDate = Instant.now();
 
     @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name="stock_date", columnDefinition = "DATE NOT NULL")
+    private LocalDate stockDate = LocalDate.now();
+
+    @NotNull
     @Column(name="is_bought", columnDefinition = "BOOLEAN NOT NULL")
     private boolean isBought;
 
     @NotNull
     @Column(name="stock_price", columnDefinition = "FLOAT NOT NULL")
-    private float stockPrice;
+    private double stockPrice;
 
     @NotNull
     @Column(name="stock_amount", columnDefinition = "INT NOT NULL")
@@ -57,7 +63,7 @@ public class Journal {
 
     public Journal() {}
 
-    public Journal(String tickerSymbol, boolean isBought, float stockPrice, int stockAmount, Map<String, List<Double>> jsonBigFiveNumber, @NotNull String memo, @NotNull AppUser appUser) {
+    public Journal(String tickerSymbol, boolean isBought, double stockPrice, int stockAmount, Map<String, List<Double>> jsonBigFiveNumber, @NotNull String memo, @NotNull AppUser appUser) {
         this.tickerSymbol = tickerSymbol;
         this.isBought = isBought;
         this.stockPrice = stockPrice;
@@ -87,7 +93,7 @@ public class Journal {
         return lastEditDate;
     }
 
-    public void setLastEditDate(Instant lastEditDate) {
+    public void setLastEditDate(@NotNull Instant lastEditDate) {
         this.lastEditDate = lastEditDate;
     }
 
@@ -99,11 +105,11 @@ public class Journal {
         isBought = bought;
     }
 
-    public float getStockPrice() {
+    public double getStockPrice() {
         return stockPrice;
     }
 
-    public void setStockPrice(float stockPrice) {
+    public void setStockPrice(double stockPrice) {
         this.stockPrice = stockPrice;
     }
 
@@ -119,7 +125,7 @@ public class Journal {
         return jsonBigFiveNumber;
     }
 
-    public void setJsonBigFiveNumber(Map<String, List<Double>> jsonBigFiveNumber) {
+    public void setJsonBigFiveNumber(@NotNull Map<String, List<Double>> jsonBigFiveNumber) {
         this.jsonBigFiveNumber = jsonBigFiveNumber;
     }
 
@@ -127,15 +133,23 @@ public class Journal {
         return memo;
     }
 
-    public void setMemo(String memo) {
+    public void setMemo(@NotNull String memo) {
         this.memo = memo;
+    }
+
+    public LocalDate getStockDate() {
+        return stockDate;
+    }
+
+    public void setStockDate(@NotNull LocalDate stockDate) {
+        this.stockDate = stockDate;
     }
 
     public AppUser getAppUser() {
         return appUser;
     }
 
-    public void setAppUser(AppUser appUser) {
+    public void setAppUser(@NotNull AppUser appUser) {
         this.appUser = appUser;
     }
 
@@ -145,10 +159,11 @@ public class Journal {
                 "id=" + id +
                 ", tickerSymbol='" + tickerSymbol + '\'' +
                 ", lastEditDate=" + lastEditDate +
+                ", stockDate=" + stockDate +
                 ", isBought=" + isBought +
                 ", stockPrice=" + stockPrice +
                 ", stockAmount=" + stockAmount +
-                ", jsonBigFiveNumber='" + jsonBigFiveNumber + '\'' +
+                ", jsonBigFiveNumber=" + jsonBigFiveNumber +
                 ", memo='" + memo + '\'' +
                 ", appUser=" + appUser +
                 '}';
